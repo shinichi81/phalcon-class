7115490665ebf2a2197ea0bc1c31cecdd5487d21
Sergey <ustasby@gmail.com>
2014-06-15 17:33:56 +0300

    Create Auth.php

---
+++ Auth.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Board;
+
+use Phalcon\Mvc\User\Component;
+
+class Auth extends Component
+{
+
+    public function getToken($tokenKey, $lifetime = 0, $numberBytes = null)
+    {
+        if (null === $numberBytes) {
+            $numberBytes = 12;
+        }
+
+        if (0 !== $lifetime) {
+            $lifetime = \time() + $lifetime;
+        }
+
+        if (false === function_exists('openssl_random_pseudo_bytes')) {
+            throw new \Exception('Openssl extension must be loaded');
+        }
+        $token = \md5(openssl_random_pseudo_bytes($numberBytes));
+
+        $this->session->set('$Token-' . $tokenKey, $token);
+        $this->session->set('$TokenTime-' . $tokenKey, $lifetime);
+
+        return $token;
+    }
+
+    public function checkToken($tokenKey, $tokenValue)
+    {
+        $token = $this->getSessionToken($tokenKey);
+        if (null !== $token) {
+            if ($tokenValue === $token) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getSessionToken($tokenKey)
+    {
+        if ($this->session->has('$Token-' . $tokenKey)) {
+            $token = $this->session->get('$Token-' . $tokenKey, null);
+            $lifetime = (int)$this->session->get('$TokenTime-' . $tokenKey, 0);
+            if (0 === $lifetime) {
+                return $token;
+            }
+
+            $time = \time();
+            if ($time < $lifetime) {
+                return $token;
+            }else {
+                $this->session->remove('$Token-' . $tokenKey);
+                $this->session->remove('$TokenTime-' . $tokenKey);
+            }
+        }
+        return null;
+    }
+
+    public function passwordHash($password)
+    {
+        return \password_hash($password, PASSWORD_DEFAULT);
+    }
+
+    public function passwordVerify($password , $hash)
+    {
+        return \password_verify($password , $hash);
+    }
+
+    public function passwordNeedsRehash($hash)
+    {
+        return \password_needs_rehash ($hash, PASSWORD_DEFAULT);
+    }
+
+    public function passwordGetInfo($hash)
+    {
+        return \password_get_info($hash);
+
+    }
+
+
+
+
+}
+
+if (!defined('PASSWORD_DEFAULT')) {
+    require(__DIR__ . '/PasswordCompat.php');
+}
